5ea3e824c221f5606c99affaff266966028418d5
koba-e964 <koba_max@kobas-Air.pohe>
2017-08-17 08:18:27 +0900

    Add calc-test.lean

---
+++ calc-test.lean
@@ -0,0 +1,37 @@
+example : forall n m: nat, n + m = m + n :=
+begin
+  intros n,
+  induction n with n' ih,
+  show forall m, 0 + m = m + 0,
+  {
+    intro m,
+    induction m with m' ih2,
+    show 0 + 0 = 0 + 0, { reflexivity },
+    show 0 + (m' + 1) = (m' + 1) + 0,
+    calc
+      0 + (m' + 1) = (0 + m') + 1 : by reflexivity
+              ...  = (m' + 0) + 1 : by rw ih2
+              ...  = m' + 1 : by reflexivity
+              ...  = (m' + 1) + 0 : by reflexivity
+  },
+  show forall m, (n' + 1) + m = m + (n' + 1),
+  intro m,
+  induction m with m' ih2,
+  show (n' + 1) + 0 = 0 + (n' + 1),
+  {
+    calc
+      (n' + 1) + 0 = n' + 1 : by simp
+               ... = (n' + 0) + 1 : by simp
+               ... = (0 + n') + 1 : by rw ih
+               ... = 0 + (n' + 1) : by simp
+  },
+  show (n' + 1) + (m' + 1) = (m' + 1) + (n' + 1),
+  calc
+    (n' + 1) + (m' + 1) = (n' + 1) + m' + 1 : by reflexivity
+                   ...  = m' + (n' + 1) + 1 : by rw ih2
+                   ...  = m' + n' + 2 : by reflexivity
+                   ...  = n' + m' + 2 : by rw -ih
+                   ...  = n' + (m' + 1) + 1 : by reflexivity
+                   ...  = (m' + 1) + n' + 1 : by rw ih
+                   ...  = (m' + 1) + (n' + 1) : by reflexivity
+end
